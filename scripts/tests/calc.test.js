265a03f8a7603af37e2394a8882727d6cc42f734
Martin Bradbury <martinbradbury@btinternet.com>
2024-01-29 13:31:25 +0000

    initial tests for addition added and function for the addition test.

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,21 @@
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 80 for 20 + 20", () => {
+            expect(addition(40, 40)).toBe(80);
+        });
+    });
+    describe("Subtract function", () => {
+
+    });
+    describe("Multiply function", () => {
+
+    });
+    describe("Divide function", () => {
+
+    });
+});
