3617f8cb214c809e00ba97d8174d3313fd457665
Martin Bradbury <martinbradbury@btinternet.com>
2024-01-30 10:03:38 +0000

    added fs to allow multiple tests in button

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -4,17 +4,21 @@
 
 const buttonClick = require("../button");
 
-describe("DOM Test", () => {
-    beforeEach(() => {
-        document.body.innerHTML = '<p id="par"></p>'; // Load only this part of the DOM to test
-    });
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
 
-    afterEach(() => {
-        document.body.innerHTML = ''; // Clean up the DOM after each test
+describe("DOM tests", () => {
+    test("Expects content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par")
+            .innerHTML).toEqual("You Clicked");
     });
-
-    test("expect p content to change", () => {
-        buttonClick(); // calls the button click function
-        expect(document.getElementById('par').innerHTML).toEqual("You Clicked");
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
